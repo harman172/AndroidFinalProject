9d3668f9cecfd002a35cbd2ba13bc8b593b07aaf
Harman Kaur <kaurharmanpreet502@gmail.com>
2019-12-11 23:03:33 -0500

    ScrollView added

--- app/src/main/java/com/example/harman_c0765590_fp/MainActivity.java
+++ app/src/main/java/com/example/harman_c0765590_fp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.harman_c0765590_fp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btn_add);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
