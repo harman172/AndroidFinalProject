8ca7f0fca7b555f257e090f4ae9e94f7ab8d513f
Harman Kaur <kaurharmanpreet502@gmail.com>
2019-12-13 01:06:32 -0500

    layout of custom adapter for listview

--- app/src/main/java/com/example/harman_c0765590_fp/MainActivity.java
+++ app/src/main/java/com/example/harman_c0765590_fp/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnAdd;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnAdd = findViewById(R.id.btn_add);
+        listView = findViewById(R.id.emp_list);
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
